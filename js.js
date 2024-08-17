8bc129864d748cee9b367cfd60bfdd542438da2e
MaiGalili <maigalili@gmail.com>
2024-08-17 12:27:03 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+'use script'
+
+//question 1
+function addClass() {
+    const input = document.getElementById('classInput').value;
+    const text = document.getElementById('text');
+
+    text.className ='';
+
+    text.classList.add(input);
+}
+
+//question 2
+function toggleParagraph(paragraphId) {
+    const paragraph = document.getElementById(paragraphId);
+    if (paragraph.style.display === 'none') {
+        paragraph.style.display = 'block';
+    } else {
+        paragraph.style.display = 'none';
+    }
+}
+
+//question 3
+function handleClickMe() {
+    const button = document.getElementById('clickMeButton');
+    button.innerText = 'Clicked';
+    button.classList.add('disabled');
+}        
